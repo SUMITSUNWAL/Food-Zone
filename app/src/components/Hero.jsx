aa9697a3bc3c707a2329759f6b7ac77941ce7c7f
Sumit <nnm22cs184@nmamit.in>
2025-01-02 23:46:04 +0530

    Background Image Blured

--- app/src/components/Hero.jsx
+++ app/src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
-    <div className="h-[calc(100vh-160px)] bg-[url('/images/bg-image.png')] bg-cover opacity-[0.4]">
-      <div>Hii there</div>
+    <div  className="h-[calc(100vh-160px)]  relative before:content-[''] before:absolute before:bg-[url('/images/bg-image.png')] before:top-0 before:bottom-0 before:right-0 before:left-0 before:opacity-50">
+      <div className="relative">Hii there</div>
       </div>
   );
 };
